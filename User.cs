321aba6b62280cea1f763b908731da8ecdfa10d3
Студент группы IS <is@sielom.local>
2025-04-04 15:10:52 +0500

    new ver2

--- User.cs
+++ User.cs
@@ -17,6 +17,14 @@ namespace WebApplication7
         public string Login { get; set; }
         [Column("password")]
         public string Password { get; set; }
+        [Column("city_id")]
+        public int? city_id { get; set; }
     }
-
+    public class City: BaseModel
+    {
+        [Column("id")]
+        public int id { get; set; }
+        [Column("name")]
+        public string name { get; set; }
+    }
 }
